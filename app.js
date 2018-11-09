2c3e4c4112455204cbf64e795d48b8c5119dd6c9
Luciano Milen <lucianomilen@dcc.ufmg.br>
2018-11-09 20:09:26 -0200

    style refactor; adding google analytics (fail for now)

--- app.js
+++ app.js
@@ -1,109 +1,101 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-129010212-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+// create element
 const div = document.createElement('div')
 const vid = document.createElement('div')
-const hook = document.createElement('div')
+const arrowContainerElement = document.createElement('div')
+const leftArrowElement = document.createElement('img')
+const rightArrowElement = document.createElement('img')
 
-div.id = 'sideDiv'
+// set img src
+rightArrowElement.src = chrome.extension.getURL("images/right-chevron.svg")
+leftArrowElement.src = chrome.extension.getURL("images/left-chevron.svg")
 
-vid.id = 'contentDiv'
+// jquery queries
+const openButton = $(div)
+const contentView = $(vid)
+const leftArrow = $(leftArrowElement)
+const rightArrow = $(rightArrowElement)
+const arrowContainer = $(arrowContainerElement)
 
-hook.id = 'hookDiv'
-
-const sideDivStyle = {
-    position: "fixed",
-    display: "flex",
-    alignItems: "center",
-    fontWeight: "bold",
-    justifyContent: "center",
-    width: "40px",
-    height: "40px",
-    cursor: "pointer",
-    backgroundColor: "lightgray",
-    zIndex: "1000",
-    right: "0",
-    top: "50%",
-    borderTopLeftRadius: "155px",
-    borderBottomLeftRadius: "155px"
-}
-
-const ContentDivStyle = {
-    position: "fixed",
-    display: "none",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "300px",
-    height: "200px",
-    backgroundColor: "#ddd",
-    zIndex: "1000",
-    right: "0",
-    top: "calc(50% - 80px)",
-    boxShadow: "0 0 1px 0px #4c4c4c"
-}
-
-const chartContainerStyle = {
-    width: '100%',
-    height: '100%'
-}
-
-$(div).css(sideDivStyle)
-$(vid).css(ContentDivStyle)
-
-$(div).click(() => {
-    if ($(vid).is(":visible")) {
-        $(div).text("←")
-        $(vid).css("display", "none")
-        $(div).css("right", "0")
-    }
-    else {
-        $(div).text("→")
-        $(div).css("right", "300px")
-        $(vid).css("display", "flex")
-    }
-
-})
-
-const config = {
+// constants
+const lowLMAColor = "#ff6a00"
+const mediumLMAColor = "#d5ff2f"
+const highLMAColor = "#28a745"
+const firebaseConfig = {
     apiKey: "AAAA5vjQHxs:APA91bHUflcKyy9kkLxu7Q--trmLWgp8mP6QAvuCpqM5ffEmkVKF6kSRRiKhz4bJENwFL2UvZHCaveWGH7gtHrA_3Gp03ztcOgiSoJeZ4pW06HeJttTbPEuL515_enXK25fSd2QpvTKW",
     authDomain: "992016867099.firebaseapp.com",
     databaseURL: "https://level-maintenance-plugin.firebaseio.com",
     storageBucket: "level-maintenance-plugin.appspot.com",
 }
 
-firebase.initializeApp(config)
+// class adding
+rightArrow.addClass("arrowIcon")
+leftArrow.addClass("arrowIcon")
+openButton.addClass("openButton")
+contentView.addClass("contentView")
 
-const gaugeTitleStyle = {
-    textAlign: 'center',
-    position: 'absolute',
-    top: 0
-}
+// firebase init
+firebase.initializeApp(firebaseConfig)
 
-function setArrowBackground() {
-    if(value < 25) {
-        $(div).css("background-color", "#bf3500")
-    }
-    else if(value < 75) {
-        $(div).css("background-color", "#ceff00")
+// events
+openButton.click((e) => {
+    if (contentView.is(":visible")) {
+        arrowContainer.html(leftArrow)
+        contentView.css("display", "none")
+        openButton.css("right", "0")
     }
     else {
-        $(div).css("background-color", "#00ff00")
+        console.log(_gaq, e);
+        _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+        arrowContainer.html(rightArrow)
+        openButton.css("right", "300px")
+        contentView.css("display", "flex")
+    }
+
+})
+
+// functions
+function setArrowBackground(value) {
+    if(value < 0) {
+        openButton.css("background-color", "#bf3500")
+    }
+    else if(value < 25) {
+        openButton.css("background-color", lowLMAColor)
+    }
+    else if(value < 75) {
+        openButton.css("background-color", mediumLMAColor)
+    }
+    else {
+        openButton.css("background-color", highLMAColor)
     }
 }
 
-
-function createChart(result, repName) {
-    console.log(repName)
-    let chartContainer = document.createElement('div')
-    chartContainer.id = 'gauge'
-    $(chartContainer).css(chartContainerStyle)
+function setContentTitle(repName) {
     const title = document.createElement('p')
-    $(title).css(gaugeTitleStyle)
+    $(title).addClass("gaugeTitle")
     $(title).text(repName)
-    $(vid).append(title)
-    $(vid).append(chartContainer)
+    contentView.append(title)
+}
 
+
+function createChart(result) {
+    let chartContainer = document.createElement("div")
+    chartContainer.id = "gauge"
+    $(chartContainer).addClass("chartContainer")
+    contentView.append(chartContainer)
 
     const g = new JustGage({
         id: "gauge",
-        value: result.lma,
+        value: result,
         min: 0,
         max: 100,
         width: 300,
@@ -116,15 +108,15 @@ function createChart(result, repName) {
         customSectors: {
             percents: true,
             ranges: [{
-                color: "#bf3500",
+                color: lowLMAColor,
                 lo: 0,
                 hi: 24
             }, {
-                color: "#ceff00",
+                color: mediumLMAColor,
                 lo: 25,
                 hi: 74
             }, {
-                color: "#00ff00",
+                color: highLMAColor,
                 lo: 75,
                 hi: 100
             }]
@@ -134,23 +126,32 @@ function createChart(result, repName) {
 
 function handleMessage(request, sender, sendResponse) {
     const {full_name} = request
-    const owner = full_name.split('/')[0]
-    const name = full_name.split('/')[1]
+    const owner = full_name.split("/")[0]
+    const name = full_name.split("/")[1]
 
-    firebase.database().ref('/projects').once('value').then((res) => {
+    firebase.database().ref("/projects").once("value").then((res) => {
             const projectOwner = res.val()[`${owner}`]
             if (projectOwner) {
-                const result = (projectOwner[`${name}`])
-                setArrowBackground(result.lma)
-                createChart(result, full_name)
+                const result = (projectOwner[`${name}`]).lma
+                setArrowBackground(result)
+                setContentTitle(full_name)
+                if(result > 0)
+                    createChart(result)
+                else 
+                    contentView.append("<p>Seems to be unmaintained...</p>")
+                
             }
-            else console.log("not found")
+            else {
+                openButton.addClass("openButtonGray")
+                contentView.append("<p>This project hasn't been analysed yet...</p>")
+            }
         }
     )
 
     $('.repohead h1').after(div)
-    $(div).text("←")
-    $(div).after(vid)
+    openButton.append(arrowContainer)
+    arrowContainer.html(leftArrow)
+    openButton.after(vid)
     return true
 }
 
